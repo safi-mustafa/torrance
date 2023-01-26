d4d7c771e594cf5ca83885afba46d988fc4e3879
Ammad Khalid Butt <ammad@centangle.com>
2023-01-26 18:55:32 +0500

    Migrations added for FolderId in Attachment

---
+++ DataLibrary/Migrations/20230126135127_Adding FolderId in Attachments.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingFolderIdinAttachments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "FolderId",
+                table: "Attachments",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attachments_FolderId",
+                table: "Attachments",
+                column: "FolderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attachments_Folders_FolderId",
+                table: "Attachments",
+                column: "FolderId",
+                principalTable: "Folders",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attachments_Folders_FolderId",
+                table: "Attachments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Attachments_FolderId",
+                table: "Attachments");
+
+            migrationBuilder.DropColumn(
+                name: "FolderId",
+                table: "Attachments");
+        }
+    }
+}
