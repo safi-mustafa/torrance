cc5cc3f2bcdfd8f52c64353fc6454d4d0eab105f
Ammad Khalid Butt <ammad@centangle.com>
2023-02-01 21:42:57 +0500

    Override log tested in api

--- TimeKeepingApi/Controllers/OverrideLogController.cs
+++ TimeKeepingApi/Controllers/OverrideLogController.cs
@@ -1,5 +1,7 @@
 ﻿
 using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Repositories.Services.CommonServices;
 using Repositories.Services.OverrideLogServices.ORLogService;
 using TorranceApi.Controllers;
@@ -8,6 +10,9 @@ using ViewModels.OverrideLogs.ORLog;
 
 namespace API.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
     public class OverrideLogController : CrudBaseBriefController<ORLogModifyViewModel, ORLogModifyViewModel, ORLogDetailViewModel, ORLogDetailViewModel, ORLogSearchViewModel>
     {
         public OverrideLogController(IORLogService<ORLogModifyViewModel, ORLogModifyViewModel, ORLogDetailViewModel> ORLogService) : base(ORLogService)
--- TimeKeepingApi/Mapper/Mapping.cs
+++ TimeKeepingApi/Mapper/Mapping.cs
@@ -337,6 +337,21 @@ namespace TorranceApi.Mapper
                 .ForMember(d => d.Id, s => s.MapFrom(s => s.Employee.Id))
                 .ForMember(dest => dest.Id, act => act.Condition(src => (src.Employee != null)))
                 .ReverseMap();
+            CreateMap<ORLogModifyViewModel, OverrideLog>()
+                .ForMember(src => src.ContractorId, opt => opt.MapFrom(dest => dest.Contractor.Id))
+                .ForMember(x => x.Contractor, opt => opt.Ignore())
+                .ForMember(src => src.ShiftId, opt => opt.MapFrom(dest => dest.Shift.Id))
+                .ForMember(x => x.Shift, opt => opt.Ignore())
+                .ForMember(src => src.CraftRateId, opt => opt.MapFrom(dest => dest.CraftRate.Id))
+                .ForMember(x => x.CraftRate, opt => opt.Ignore())
+                .ForMember(src => src.CraftSkillId, opt => opt.MapFrom(dest => dest.CraftSkill.Id))
+                .ForMember(x => x.CraftSkill, opt => opt.Ignore())
+                .ForMember(src => src.ReasonForRequestId, opt => opt.MapFrom(dest => dest.ReasonForRequest.Id))
+                .ForMember(x => x.ReasonForRequest, opt => opt.Ignore())
+                .ForMember(src => src.OverrideTypeId, opt => opt.MapFrom(dest => dest.OverrideType.Id))
+                .ForMember(x => x.OverrideType, opt => opt.Ignore())
+                .ForMember(x => x.Employees, opt => opt.Ignore())
+                .ReverseMap();
             CreateMap<ORLogDetailViewModel, OverrideLog>()
                 //.ForMember(x => x.Employees, opt => opt.Ignore())
                 .ReverseMap();
