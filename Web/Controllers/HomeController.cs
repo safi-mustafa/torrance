f4935cdc98ec201cda3add226b55fdd19dfdca55
waseem <waseemsafdar98@gmail.com>
2023-02-01 17:15:22 +0500

    added dummy charts in dashboard

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -31,5 +31,43 @@ namespace Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public ActionResult GetProgressChartData()
+        {
+            try
+            {
+                            Object[] data = {
+                    new { x = "Jan", value1 = 0.5,value2 = 1, value3 = 1.2},
+                    new { x = "Feb", value1 = 1,value2 = 1,value3 = 2.2},
+                    new { x = "March", value1 = 0.5,value2 = 1, value3 = 3.2},
+                    new { x = "April", value1 = 2,value2 = 1, value3 = 4.2}
+                };
+                return Json(data);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public ActionResult GetPieChartdata()
+        {
+            try
+            {
+                Object[] data = {
+                    new { x = "Shift1", value1 = 2},
+                      new { x = "Shift2", value1 = 4},
+
+                };
+                return Json(data);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
     }
 }
