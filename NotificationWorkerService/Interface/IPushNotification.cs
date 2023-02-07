5c19282d7c3cfb3516ae62227449ffa66dae3928
Ammad Khalid Butt <ammad@centangle.com>
2023-02-07 14:45:01 +0500

    pushing to test something on fizzah's end

---
+++ NotificationWorkerService/Interface/IPushNotification.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace NotificationWorkerService.Interface
+{
+	public interface IPushNotification
+	{
+	}
+}
+
---
+++ NotificationWorkerService/Repository/PushNotificationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace NotificationWorkerService.Repository
+{
+    public class PushNotificationService
+    {
+        public PushNotificationService()
+        {
+        }
+        public async Task SendPushNotification()
+        {
+            IPushApiClient _client = new PushApiClient("your token here");
+            PushTicketRequest pushTicketRequest = new PushTicketRequest()
+            {
+                PushTo = new List<string>() { ... },
+                PushTitle = "TEST 1",
+                PushBody = "TEST 1",
+                PushChannelId = "test"
+            };
+
+            PushTicketResponse result = await _client.SendPushAsync(pushTicketRequest);
+        }
+    }
+
+}
+
