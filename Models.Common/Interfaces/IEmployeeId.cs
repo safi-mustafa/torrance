d3d34406b2b1c2dc0ce1627d4f7041c8e09266e7
Ammad Khalid Butt <ammad@centangle.com>
2023-02-07 12:06:46 +0500

    Added notification method in ApproveBaseService

---
+++ Models.Common/Interfaces/IEmployeeId.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Models.Common.Interfaces
+{
+    public interface IEmployeeId
+    {
+
+        long? EmployeeId { get; set; }
+    }
+}
+
--- ViewModels/Notification/NotificationViewModel.cs
+++ ViewModels/Notification/NotificationViewModel.cs
@@ -1,13 +1,41 @@
 ﻿using Enums;
+using Helpers.Models.Shared;
+using Models.Common.Interfaces;
+using Models.OverrideLogs;
+using Models.TimeOnTools;
+using Newtonsoft.Json;
 
 namespace ViewModels.Notification
 {
     public class NotificationViewModel
     {
+        public NotificationViewModel()
+        {
+
+        }
+        public NotificationViewModel(Status status, long logId, Type logType, string sendTo, string message)
+        {
+            var pushNotification = new PushNotificationViewModel
+            {
+                LogId = logId,
+                LogType = GetLogType(logType),
+                Message = message
+            };
+
+            EntityId = logId;
+            Message = JsonConvert.SerializeObject(pushNotification);
+            SendTo = sendTo;
+        }
         public long EntityId { get; set; }
         public string Message { get; set; }
         public string Subject { get; set; }
         public string SendTo { get; set; }
         public NotificationType Type { get; set; }
+
+
+        private LogType GetLogType(Type entity)
+        {
+            return entity is TOTLog ? LogType.TimeOnTools : entity is OverrideLog ? LogType.Override : LogType.WeldingRodRecord;
+        }
     }
 }
