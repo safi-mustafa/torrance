d93c2eeb9273b1bddcaec74b1d5811d1040dedda
fizzah <fizzah@centangle.com>
2023-04-28 12:47:20 +0500

    getAll method sorted by name asc for definitons

--- ViewModels/TimeOnTools/ReworkDelay/ReworkDelaySearchViewModel.cs
+++ ViewModels/TimeOnTools/ReworkDelay/ReworkDelaySearchViewModel.cs
@@ -4,6 +4,11 @@ namespace ViewModels.TimeOnTools.ReworkDelay
 {
     public class ReworkDelaySearchViewModel : BaseSearchModel
     {
+        public ReworkDelaySearchViewModel()
+        {
+            OrderByColumn = "Name";
+            OrderDir = PaginationOrderCatalog.Asc;
+        }
         public string Name { get; set; }
     }
 }
